4d2bfe428d9bd1974b9424006662f507d1d3d5e1
zeros <erwin.plaza.x@gmail.com>
2018-08-01 02:14:41 -0400

    It building base structure for the activities, It addition designed material dependence

---
+++ app/src/main/java/com/zeros/nano/debtzeros/activity/BaseAppCompatNoDrawerActivity.java
@@ -0,0 +1,4 @@
+package com.zeros.nano.debtzeros.activity;
+
+public class BaseAppCompatNoDrawerActivity extends BaseActivity {
+}
---
+++ app/src/main/java/com/zeros/nano/debtzeros/activity/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.zeros.nano.debtzeros.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zeros.nano.debtzeros.R;
+
+public class SplashActivity extends BaseAppCompatNoDrawerActivity {
+
+
+    Runnable splashTask = new Runnable() {
+        @Override
+        public void run() {
+            navigate();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getData();
+    }
+
+    // Revisa e iniciliza los datos, luego lanza
+    private void getData(){
+        new Handler().postAtTime( splashTask,5000);
+    }
+
+
+    private void navigate() {
+         startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+    }
+}
